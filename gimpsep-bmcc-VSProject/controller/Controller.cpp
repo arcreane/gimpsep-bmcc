3dc486388438e63cbf4f144a7eca6d3c24bd882a
BenjaminBrehier <benbrehier@gmail.com>
2024-06-11 00:48:20 +0200

    fix: image import

--- gimpsep-bmcc-VSProject/controller/Controller.cpp
+++ gimpsep-bmcc-VSProject/controller/Controller.cpp
@@ -4,10 +4,9 @@
 
 Controller::Controller() : view(model), lowThreshold(100), highThreshold(200), kernelSize(3)
 {
-    // Set the mouse callback
     view.setMouseCallback([this](int event, int x, int y, int flags, void *userdata)
                           { this->handleMouseEvent(event, x, y, flags, userdata); });
-    updateView(); // Initial update
+    updateView();
 }
 
 void Controller::handleMouseEvent(int event, int x, int y, int flags, void *userdata)
@@ -18,53 +17,42 @@ void Controller::handleMouseEvent(int event, int x, int y, int flags, void *user
         const auto& buttonRects = view.getButtonRects();
         if (buttonRects.size() > 0 && buttonRects[0].contains(cv::Point(x, y)))
         {
-            std::cout << "Load image button clicked" << std::endl;
             loadImage();
         }
         else if (buttonRects.size() > 1 && buttonRects[1].contains(cv::Point(x, y)))
         {
-            std::cout << "Save image button clicked" << std::endl;
             saveImage();
-
         }
         else if (buttonRects.size() > 2 && buttonRects[2].contains(cv::Point(x, y)))
         {
-            std::cout << "Toggle gray mode button clicked" << std::endl;
             toggleGrayMode();
         }
         else if (buttonRects.size() > 3 && buttonRects[3].contains(cv::Point(x, y)))
         {
-            std::cout << "Lighten button clicked" << std::endl;
             applyLighten();
         }
         else if (buttonRects.size() > 4 && buttonRects[4].contains(cv::Point(x, y)))
         {
-            std::cout << "Darken button clicked" << std::endl;
             applyDarken();
         }
         else if (buttonRects.size() > 5 && buttonRects[5].contains(cv::Point(x, y)))
         {
-            std::cout << "Size + mode button clicked" << std::endl;
             increaseImageSize();
         }
-        else if (buttonRects.size() > 5 && buttonRects[6].contains(cv::Point(x, y)))
+        else if (buttonRects.size() > 6 && buttonRects[6].contains(cv::Point(x, y)))
         {
-            std::cout << "Size - mode button clicked" << std::endl;
-                decreaseImageSize();
+            decreaseImageSize();
         }
-        else if (buttonRects.size() > 5 && buttonRects[7].contains(cv::Point(x, y)))
+        else if (buttonRects.size() > 7 && buttonRects[7].contains(cv::Point(x, y)))
         {
-            std::cout << "Erode threshold button clicked" << std::endl;
-                erodeOrDilate(true);
+            erodeOrDilate(true);
         }
-        else if (buttonRects.size() > 5 && buttonRects[8].contains(cv::Point(x, y)))
+        else if (buttonRects.size() > 8 && buttonRects[8].contains(cv::Point(x, y)))
         {
-            std::cout << "dilate threshold button clicked" << std::endl;
             erodeOrDilate(false);
         }
-        else if (buttonRects.size() > 5 && buttonRects[9].contains(cv::Point(x, y)))
+        else if (buttonRects.size() > 9 && buttonRects[9].contains(cv::Point(x, y)))
         {
-            std::cout << "Increase Canny threshold button clicked" << std::endl;
             applyCanny();
         }
     }
@@ -72,11 +60,10 @@ void Controller::handleMouseEvent(int event, int x, int y, int flags, void *user
 
 void Controller::loadImage()
 {
-    const char *filterPatterns[] = {"*.jpg", "*.png"};                                           // Séparez les filtres par des virgules
-    const char *imagePath = tinyfd_openFileDialog("Open Image", "", 2, filterPatterns, NULL, 0); // Le 2 indique le nombre de motifs de filtre
+    const char *filterPatterns[] = {"*.jpg", "*.png"};
+    const char *imagePath = tinyfd_openFileDialog("Open Image", "", 2, filterPatterns, NULL, 0);
     if (imagePath)
     {
-        std::cout << "Image loaded: " << imagePath << std::endl;
         model.loadImage(imagePath);
         updateView();
     }
@@ -92,26 +79,20 @@ void Controller::toggleGrayMode()
     if (!image.empty())
     {
         cv::Mat grayImage;
-        // Convert to grayscale if the image is not already in grayscale
-        // deprecated, because the image is always passed on BGR after the operation to be printed on the canvas
         if (image.channels() == 3)
         {
             cv::cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
-            std::cout << "Converted to grayscale, channels: " << grayImage.channels() << std::endl;
         }
         else
         {
-            grayImage = image.clone();  // Ensure we have a proper clone of the grayscale image
-            std::cout << "Image is already grayscale, channels: " << grayImage.channels() << std::endl;
+            grayImage = image.clone();
         }
 
         if (!grayImage.empty())
         {
             model.setImage(grayImage);
-            // To give image the same type as canvas to print it
             cv::cvtColor(model.getImage(), grayImage, cv::COLOR_GRAY2BGR);
             model.setImage(grayImage);
-            std::cout << "Image type after gray conversion: " << model.getImage().type() << std::endl;
             updateView();
         }
         else
@@ -125,54 +106,53 @@ void Controller::toggleGrayMode()
     }
 }
 
-
 void Controller::increaseImageSize()
 {
     cv::Mat image = model.getImage();
-    if (!image.empty()) {
-        // Diminue la taille de l'image par exemple en divisant la largeur et la hauteur par un facteur
-        double scaleFactor = 1.1f; // Facteur de réduction par exemple
+    if (!image.empty())
+    {
+        double scaleFactor = 1.1f;
         cv::Mat resizedImage;
         cv::resize(image, resizedImage, cv::Size(), scaleFactor, scaleFactor);
-        image = resizedImage;
         model.setImage(resizedImage);
-        if (!model.isResizeMode()) {
+        if (!model.isResizeMode())
+        {
             model.toggleResizeMode();
         }
-        updateView(); // Met à jour la vue avec la nouvelle image
+        updateView();
     }
 }
 
-void Controller::decreaseImageSize() {
+void Controller::decreaseImageSize()
+{
     cv::Mat image = model.getImage();
-    if (!image.empty()) {
-        // Diminue la taille de l'image par exemple en divisant la largeur et la hauteur par un facteur
-        double scaleFactor = 0.9f; // Facteur de réduction par exemple
+    if (!image.empty())
+    {
+        double scaleFactor = 0.9f;
         cv::Mat resizedImage;
         cv::resize(image, resizedImage, cv::Size(), scaleFactor, scaleFactor);
-        image = resizedImage;
-        std::cout << "Image type after resize" << resizedImage.type() << std::endl;
         model.setImage(resizedImage);
-        std::cout << "Image type after save" << model.getImage().type() << std::endl;
-        if (!model.isResizeMode()) {
+        if (!model.isResizeMode())
+        {
             model.toggleResizeMode();
         }
-        std::cout << "new Width : " << static_cast<int>(model.getImage().cols) << std::endl;
-        updateView(); // Met à jour la vue avec la nouvelle image
+        updateView();
     }
 }
 
 void Controller::applyLighten()
 {
     cv::Mat lightenedImage = model.getImage();
-    lightenedImage.convertTo(lightenedImage, -1, 1, 50); // Increase brightness
+    lightenedImage.convertTo(lightenedImage, -1, 1, 50);
+    model.setImage(lightenedImage);
     updateView();
 }
 
 void Controller::applyDarken()
 {
     cv::Mat darkenImage = model.getImage();
-    darkenImage.convertTo(darkenImage, -1, 1, - 50); // Increase brightness
+    darkenImage.convertTo(darkenImage, -1, 1, -50);
+    model.setImage(darkenImage);
     updateView();
 }
 
@@ -191,17 +171,16 @@ void Controller::applyCanny()
 {
     cv::Mat grayImage, edges;
 
-    // Convert to grayscale if the image is not already in grayscale
-    if (model.getImage().channels() == 3) {
+    if (model.getImage().channels() == 3)
+    {
         cv::cvtColor(model.getImage(), grayImage, cv::COLOR_BGR2GRAY);
-    } else {
+    }
+    else
+    {
         grayImage = model.getImage();
     }
 
-    // Apply Canny edge detection
     cv::Canny(grayImage, edges, lowThreshold, highThreshold, kernelSize);
-
-    // Convert edges to BGR format to display in color image
     cv::cvtColor(edges, model.getImage(), cv::COLOR_GRAY2BGR);
     updateView();
 }
@@ -226,4 +205,3 @@ void Controller::erodeOrDilate(bool isErosion)
         updateView();
     }
 }
-
--- gimpsep-bmcc-VSProject/view/View.h
+++ gimpsep-bmcc-VSProject/view/View.h
@@ -6,29 +6,27 @@
 #include <vector>
 #include "../model/ImageModel.h"
 
-class View
-{
+class View {
 public:
-    View(ImageModel &model);
-    void setMouseCallback(std::function<void(int, int, int, int, void *)> callback);
+    View(ImageModel& model);
+    void setMouseCallback(std::function<void(int, int, int, int, void*)> callback);
     void update();
-    void drawButtons(const std::vector<std::string>& buttonNames);
-    const std::vector<cv::Rect>& getButtonRects() const { return buttonRects; } // Getter for buttonRects
+    const std::vector<cv::Rect>& getButtonRects() const { return buttonRects; }
 
 private:
-    ImageModel &model;
-    cv::Mat canvas;
-    std::float_t scaleFactor;
-    std::string buttonText;
-    std::string winName;
-    std::function<void(int, int, int, int, void *)> mouseCallback; // Store the callback
-    std::vector<cv::Rect> buttonRects; // Store button positions
+    static void onMouse(int event, int x, int y, int flags, void* userdata);
+    static void onErosionSizeChange(int, void* userdata);
+    void drawButtons(const std::vector<std::string>& buttonNames);
     void createGUI();
-    static void onMouse(int event, int x, int y, int flags, void *userdata); // Static function for OpenCV callback
 
-    static void onErosionSizeChange(int, void*);
-    int erosionSize;
-    int dilationSize;
+    ImageModel& model;
+    std::string winName;
+    std::string buttonText;
+    float scaleFactor;
+    int erosionSize = 1;
+    std::vector<cv::Rect> buttonRects;
+    cv::Mat3b canvas;
+    std::function<void(int, int, int, int, void*)> mouseCallback;
 };
 
 #endif // VIEW_H
